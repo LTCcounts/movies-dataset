0fab07b5fc96f108483c7fcac1433119ec2cfdef
LTCcounts <stashalseiu2015@gmail.com>
2026-02-04 23:34:45 +0000

    data change

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write(
 @st.cache_data
 def load_data():
     #df = pd.read_csv(r'C:\Users\StashaL\Dropbox\PC\Desktop\pnwsu_overhead.csv')
-    df = pd.read_csv('data/pnwsu_overhead.csv')
+    df = pd.read_csv('data/pnwsu_overhead1.csv')
     return df
 
 
