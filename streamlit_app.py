f30fec529ae78435c4f3e187325723eba3b440bf
LTCcounts <stashalseiu2015@gmail.com>
2026-02-05 07:05:17 +0000

    uhhhh

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,9 @@ st.write(
     """
 )
 
-st.table({'Year':['Income', 'Expenses'], '2025':[479040.73, 302795.81],'2024':[376153.56, 61651.26],'2023':[352818.17, 157027.20]})  
+pnl = st.table({'Year':['Income', 'Expenses'], '2025':[479040.73, 302795.81],'2024':[376153.56, 61651.26],'2023':[352818.17, 157027.20]})  
+pnl_df = pd.DataFrame(pnl, index=false)
+st.table(pnl_df)
 
 def load_data2():
     df2 = pd.read_csv('data/pnwsu_dues.csv')
