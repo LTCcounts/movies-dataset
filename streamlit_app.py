6d945c5e3d1842d0a3ac04b11eae36480976e86a
LTCcounts <stashalseiu2015@gmail.com>
2026-02-05 07:34:40 +0000

    no button yet

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,6 @@ st.write(
     """
 )
 
-tblobj = st.table({'Year':['Income', 'Expenses'], '2025':[479040.73, 302795.81],'2024':[376153.56, 61651.26],'2023':[352818.17, 157027.20]})
-st.button("Table", on_click=tblobj)
-    
-
 
 
 
